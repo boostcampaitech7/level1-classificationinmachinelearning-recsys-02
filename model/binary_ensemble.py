fee5ff387fdbbe5dc8ed15f4fd03e433fdbae70a
seoo2001 <seoo2001@naver.com>
2024-09-28 20:23:09 +0900

    Refactor: train.py에 구현 된 모델들을 class로 구분함
    #14

--- model/binary_ensemble.py
+++ model/binary_ensemble.py
@@ -24,6 +24,7 @@ params = {
     "verbose": 0,
 }
 
+
 class BinaryEnsemble:
     def __init__(self, params=params, binary_params=binary_params):
         self.params = params
@@ -31,27 +32,45 @@ class BinaryEnsemble:
         self.lgb_model = None
         self.binary_model = None
     
-    def train(self, x_train, y_train, x_valid, y_valid):
-        # main lgb model
-        train_data = lgb.Dataset(x_train, label=y_train)
-        valid_data = lgb.Dataset(x_valid, label=y_valid, reference=train_data)
-        lgb_model = lgb.train(
-            params=self.params,
-            train_set=train_data,
-            valid_sets=valid_data,
-        )
-        # binary model
-        y_train_binary = y_train.apply(lambda x: 1 if x >= 2 else 0)
-        y_valid_binary = y_valid.apply(lambda x: 1 if x >= 2 else 0)
-        train_data_binary = lgb.Dataset(x_train, label=y_train_binary)
-        valid_data_binary = lgb.Dataset(x_valid, label=y_valid_binary, reference=train_data_binary)
-        binary_model = lgb.train(
-            params=self.binary_params,
-            train_set=train_data_binary,
-            valid_sets=valid_data_binary,
-        )
-        self.lgb_model = lgb_model
-        self.binary_model = binary_model
+    def train(self, x_train, y_train, x_valid=None, y_valid=None):
+        if valid_data is None or y_valid is None:
+            # main lgb model
+            train_data = lgb.Dataset(x_train, label=y_train)
+            lgb_model = lgb.train(
+                params=self.params,
+                train_set=train_data,
+            )
+            self.lgb_model = lgb_model
+            
+            # binary model
+            y_train_binary = y_train.apply(lambda x: 1 if x >= 2 else 0)
+            train_data_binary = lgb.Dataset(x_train, label=y_train_binary)
+            binary_model = lgb.train(
+                params=self.binary_params,
+                train_set=train_data_binary,
+            )
+            self.binary_model = binary_model
+        else:
+            # main lgb model
+            train_data = lgb.Dataset(x_train, label=y_train)
+            valid_data = lgb.Dataset(x_valid, label=y_valid, reference=train_data)
+            lgb_model = lgb.train(
+                params=self.params,
+                train_set=train_data,
+                valid_sets=valid_data,
+            )
+            # binary model
+            y_train_binary = y_train.apply(lambda x: 1 if x >= 2 else 0)
+            y_valid_binary = y_valid.apply(lambda x: 1 if x >= 2 else 0)
+            train_data_binary = lgb.Dataset(x_train, label=y_train_binary)
+            valid_data_binary = lgb.Dataset(x_valid, label=y_valid_binary, reference=train_data_binary)
+            binary_model = lgb.train(
+                params=self.binary_params,
+                train_set=train_data_binary,
+                valid_sets=valid_data_binary,
+            )
+            self.lgb_model = lgb_model
+            self.binary_model = binary_model
     
     def predict_proba(self, x_valid):
         if self.lgb_model is None or self.binary_model is None:
